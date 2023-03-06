5ff550c2bbda6507ccaa70c557bfd5cc96ba42cd
parixscm <parixscm@gmail.com>
2023-03-07 02:49:16 +0900

    🔨 Refactor: ADD function getting search results data

--- lib/getData.ts
+++ lib/getData.ts
@@ -7,3 +7,10 @@ export async function getLiveAnywhereData() {
   const res = await fetch("https://www.jsonkeeper.com/b/VHHT");
   return res.json();
 }
+
+export async function getSearchResults() {
+  const res = await fetch("https://www.jsonkeeper.com/b/5NPS", {
+    next: { revalidate: 10 },
+  });
+  return res.json();
+}

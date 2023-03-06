d8bdd25c363d1c8a9db8423a08580a0e0f662360
parixscm <parixscm@gmail.com>
2023-03-06 15:48:12 +0900

    📝 Feat: ADD footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <footer className="px-32 py-14 grid grid-cols-1 gap-10 bg-gray-100 md:grid-cols-4">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsrooms</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is a cloned site</p>
+        <p>Isn't it pretty awesome?</p>
+        <p>Referrals accepted</p>
+        <p>Parixscm</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Parixscm</p>
+        <p>Presents</p>
+        <p>Wanna be fullstack hero</p>
+        <p>Keep on studying</p>
+        <p>Never stop learning</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help Centre</p>
+        <p>Trust & Safety</p>
+        <p>Explosive</p>
+        <p>Growth</p>
+        <p>As a frontend developer</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
